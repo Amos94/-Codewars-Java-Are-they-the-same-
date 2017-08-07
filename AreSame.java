b45ece5b527de370a13369dd2dc03763dd35ea96
Amos Madalin Neculau <amos.neculau@kcl.ac.uk>
2017-08-07 17:23:22 +0300

    Update solution

--- AreSame.java
+++ AreSame.java
@@ -7,27 +7,27 @@ public class AreSame {
 		
 		boolean toReturn = true;
     
-    if(a == null || b == null){
-      return false;
-    }
-		
+		if(a == null || b == null){
+			return false;
+		}
+
 		if(a.length != b.length) {
 			return false;
 		}
 
-		
+
 		for(int i=0; i<b.length; ++i) {
 			a[i] = a[i]*a[i];
 		}
-		
-		
+
+
 		Arrays.sort(a); Arrays.sort(b);
-		
+
 		for(int i=0; i<a.length; ++i)
 			if(a[i] != b[i])
 				return false;
-		
-    return toReturn;
+
+		return toReturn;
     
-  }
+	}
 }
